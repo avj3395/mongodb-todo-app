19a29111f170bd812779e99ac3fc8ca18db650b5
Akshay <avj3395@gmail.com>
2020-11-17 12:34:25 +0530

    change in routiong

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
 import Navbars from "./components/navbar.component";
